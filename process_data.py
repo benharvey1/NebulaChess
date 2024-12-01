4f2ee9bee4fecbbf3836822c5dc9b70b6bf010f7
benharvey1 <behh2@cam.ac.uk>
2024-12-01 11:48:05 +0000

    Added progress bar to data processing

--- process_data.py
+++ process_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from Board_State import State
+from tqdm import tqdm
 
 data = pd.read_csv('TransformedData.csv')
 fen = data['FEN'].to_numpy()
@@ -14,7 +15,7 @@ y = np.zeros(num_samples, dtype=np.float32)
 
 state_converter = State()
 
-for i in range(num_samples):
+for i in tqdm(range(num_samples), desc='Processing Data', unit='Iteration'):
     state_converter.board.set_fen(fen[i])
     X[i] = state_converter.board_to_tensor() 
     y[i] = float(evaluation[i]) 
