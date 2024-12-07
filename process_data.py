0a039006ac532e12e7ad853606782e4e4988436f
benharvey1 <behh2@cam.ac.uk>
2024-12-07 15:08:15 +0000

    process portion of dataset

--- process_data.py
+++ process_data.py
@@ -4,14 +4,17 @@ from Board_State import State
 from tqdm import tqdm
 
 data = pd.read_csv('Data.csv')
+data = data.sample(frac=1, random_state=42).reset_index(drop=True)
+
 fen = data['FEN'].to_numpy()
 evaluation = data['Evaluation'].to_numpy()
 
-num_samples = len(data)
-tensor_shape = (19, 8, 8)
+portion = 0.1
+num_samples = int(len(data)*portion)
+#tensor_shape = (19, 8, 8)
 vector_shape = (901,)
 
-X_1 = np.zeros((num_samples, *tensor_shape), dtype=np.uint8)
+#X_1 = np.zeros((num_samples, *tensor_shape), dtype=np.uint8)
 X_2 = np.zeros((num_samples, *vector_shape), dtype=np.uint8)
 y = np.zeros(num_samples, dtype=np.float32)
 
@@ -19,11 +22,11 @@ state_converter = State()
 
 for i in tqdm(range(num_samples), desc='Processing Data', unit='Iteration'):
     state_converter.board.set_fen(fen[i])
-    X_1[i] = state_converter.board_to_tensor()
+    #X_1[i] = state_converter.board_to_tensor()
     X_2[i] = state_converter.board_to_vector()
     y[i] = (2/np.pi)*np.arctan(evaluation[i]/250)   # arctan transformation 
 
-np.savez_compressed('processed_tensor_data.npz', X=X_1, y=y)
+#np.savez_compressed('processed_tensor_data.npz', X=X_1, y=y)
 np.savez_compressed('processed_vector_data.npz', X=X_2, y=y)
 print("Data preprocessing complete!")
 
